959ca5b2dadbe9a0fa4bc678a93a3d7d748b65e8
Stefany Vasconcelos <stefany.vasc.sa@gmail.com>
2023-11-12 17:01:19 -0300

    add data mock

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'UI tutorial',
+        productNumber: '0001',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Experts course',
+        productNumber: '0002',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'JavaScript Mentoria',
+        productNumber: '0003',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    }
+]
